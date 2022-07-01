fd6368a67be5a8c81d9f77528f2c0991dd0e96b8
pachoGit <carlos.hcpv@gmail.com>
2022-07-01 16:43:18 -0500

    FrontEnd del formulario realizado

--- app/src/main/java/com/example/pacho/MainActivity.kt
+++ app/src/main/java/com/example/pacho/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.TextView
+import android.widget.EditText
 import android.widget.Spinner
 import android.widget.ArrayAdapter
 import kotlin.random.Random
@@ -13,10 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-	val spinner: Spinner = findViewById(R.id.ciudad)
+	val spinner: Spinner = findViewById(R.id.Spinner1)
 	ArrayAdapter.createFromResource(
             this,
-            R.array.ciudades_array,
+            R.array.estado_civil_array,
             android.R.layout.simple_spinner_item
 	).also { adapter ->
 		     // Specify the layout to use when the list of choices appears
@@ -24,6 +25,56 @@ class MainActivity : AppCompatActivity() {
 		 // Apply the adapter to the spinner
 		 spinner.adapter = adapter
 	}
+
+
+	val spinner2: Spinner = findViewById(R.id.Spinner2)
+	ArrayAdapter.createFromResource(
+            this,
+            R.array.estado_civil_array,
+            android.R.layout.simple_spinner_item
+	).also { adapter ->
+		     // Specify the layout to use when the list of choices appears
+		 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+		 // Apply the adapter to the spinner
+		 spinner2.adapter = adapter
+	}
+
+	val spinner3: Spinner = findViewById(R.id.Spinner3)
+	ArrayAdapter.createFromResource(
+            this,
+            R.array.estado_civil_array,
+            android.R.layout.simple_spinner_item
+	).also { adapter ->
+		     // Specify the layout to use when the list of choices appears
+		 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+		 // Apply the adapter to the spinner
+		 spinner3.adapter = adapter
+	}
+
+	val spinner4: Spinner = findViewById(R.id.Spinner4)
+	ArrayAdapter.createFromResource(
+            this,
+            R.array.estado_civil_array,
+            android.R.layout.simple_spinner_item
+	).also { adapter ->
+		     // Specify the layout to use when the list of choices appears
+		 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+		 // Apply the adapter to the spinner
+		 spinner4.adapter = adapter
+	}
+
+	val spinner5: Spinner = findViewById(R.id.Spinner5)
+	ArrayAdapter.createFromResource(
+            this,
+            R.array.grado_array,
+            android.R.layout.simple_spinner_item
+	).also { adapter ->
+		     // Specify the layout to use when the list of choices appears
+		 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+		 // Apply the adapter to the spinner
+		 spinner5.adapter = adapter
+	}
+
 
     }
 }
